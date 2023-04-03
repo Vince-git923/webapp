e2a27f8a88026a99c665853661a470f209443e83
Vince-git923 <111721350+Vince-git923@users.noreply.github.com>
2023-04-04 04:01:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+let powerB = document.querySelector(".powerB")
+let tempB = document.querySelector(".tempB")
+let disp = document.querySelector(".displaybox")
+let tempval = document.querySelector(".tempTag")
+let powerval = document.querySelector(".powerTag")
+let slider = document.querySelector(".out")
+let slideCont = document.querySelector(".in")
+console.log(slideCont)
+
+tempval.classList.add("hidden")
+powerval.classList.add("hidden")
+
+
+
+tempB.addEventListener("click",()=>{
+    tempB.classList.add("position")
+    powerB.classList.remove("position")
+    powerval.classList.add("hidden")
+    tempval.classList.remove("hidden")
+    
+    
+})
+powerB.addEventListener("click",()=>{
+    powerB.classList.add("position")
+    tempB.classList.remove("position")
+    tempval.classList.add("hidden");
+    powerval.classList.remove("hidden")
+    
+})
+
+
+slider.classList.add("slideback")
+
+slider.addEventListener("click",()=>{
+    slider.classList.toggle("slide")
+    if(slider.classList.contains("slide")){
+        slideCont.style.border = "4px solid red"
+        
+    }else{
+        slideCont.style.border = "4px solid green"
+        
+    }
+})
+
