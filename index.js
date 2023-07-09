d9a09cc0ccf838d00692da92e6173f111acbf77c
Vince-git923 <111721350+Vince-git923@users.noreply.github.com>
2023-07-10 05:49:43 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-let powerB = document.querySelector(".powerB")
+let gasB = document.querySelector(".gasB")
 let tempB = document.querySelector(".tempB")
 let disp = document.querySelector(".displaybox")
 let tempval = document.querySelector(".tempTag")
-let powerval = document.querySelector(".powerTag")
+let senBox = document.querySelector(".gasVal")
 let slider = document.querySelector(".out")
 let slideCont = document.querySelector(".in")
 console.log(slideCont)
 let hello = document.querySelector(".helloText")
 
 tempval.classList.add("hidden")
-powerval.classList.add("hidden")
+senBox.classList.add("hidden")
 
 
 
@@ -17,31 +17,20 @@ powerval.classList.add("hidden")
 
 tempB.addEventListener("click",()=>{
     tempB.classList.add("position")
-    powerB.classList.remove("position")
-    powerval.classList.add("hidden")
+    gasB.classList.remove("position")
+    senBox.classList.add("hidden")
     tempval.classList.remove("hidden")
     hello.style.display = "none"
     
 })
-powerB.addEventListener("click",()=>{
-    powerB.classList.add("position")
+gasB.addEventListener("click",()=>{
+    gasB.classList.add("position")
     tempB.classList.remove("position")
     tempval.classList.add("hidden");
-    powerval.classList.remove("hidden")
+    senBox.classList.remove("hidden")
     hello.style.display = "none"
 })
 
 
-slider.classList.add("slideback")
 
-slider.addEventListener("click",()=>{
-    slider.classList.toggle("slide")
-    if(slider.classList.contains("slide")){
-        slideCont.style.border = "4px solid red"
-        
-    }else{
-        slideCont.style.border = "4px solid green"
-        
-    }
-})
 
